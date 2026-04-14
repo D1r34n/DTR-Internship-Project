0091204e367816f2afa8bb12163c91a7f5fae53a
Earl <earl3jordan@gmail.com>
2026-04-14 16:08:19 +0800

    connected sign in to main page

--- index.php
+++ index.php
@@ -25,25 +25,26 @@ $logo = "images/HSN.png";
         <img src="<?php echo $logo; ?>" alt="HSN Logo" class="logo">
         <h2>Sign In</h2>
 
-        <div class="field-group">
-            <label>Email</label>
-            <input type="email" placeholder="Enter your email">
-        </div>
+        <form action="main_page.php" method="POST">
+    <div class="field-group">
+        <label>Email</label>
+        <input type="email" name="email" placeholder="Enter your email">
+    </div>
 
-        <div class="field-group">
-            <label>Password</label>
-            <input type="password" placeholder="Enter your password">
-        </div>
+    <div class="field-group">
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Enter your password">
+    </div>
 
-        <div class="remember-row">
-            <label class="remember-me">
-                <input type="checkbox"> Remember me
-            </label>
-            <a href="https://www.hsnservice.com/" target="_blank" class="forgot-password"><u>Forgot password?</u></a>
-        </div>
+    <div class="remember-row">
+        <label class="remember-me">
+            <input type="checkbox"> Remember me
+        </label>
+        <a href="https://www.hsnservice.com/" target="_blank" class="forgot-password"><u>Forgot password?</u></a>
+    </div>
 
-        <button class="btn-signin">Sign In</button>
-
+    <button type="submit" class="btn-signin">Sign In</button>
+</form>
         <div class="no-account-group">
             <label>Don't have an HSN ID? </label>
             <a href="https://www.hsnservice.com/" target="_blank" style="color: #97be41; text-decoration: none;"><u>Contact your HR admin</u></a>
