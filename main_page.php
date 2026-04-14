6f8772084cf083e19a5dff9261183514f2511b90
Raiden <edrian.evangelista@gmail.com>
2026-04-14 15:00:16 +0800

    main page navigation bar

--- main_page.php
+++ main_page.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DRT System</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
     <link rel="stylesheet" href="main_page.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-<body>
+  <body>
+    <div class="navBar">
+        <a href=""><img src="images/HSN.png" alt="DTR Logo" class="logo"></a>
+        <div class="navGreetings">
+            <a><img src="images/user_profile.svg" alt="User Icon" class="userIcon"></a>
+            <h4>Welcome, User!</h4>
+        </div>
+    </div>
     
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
 </html>
