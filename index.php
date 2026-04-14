eb7e688e69029b0387566adbc43c192aa199fe58
Earl <earl3jordan@gmail.com>
2026-04-14 16:55:04 +0800

    updated with restrictions

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
+session_start();
 $bg_image = "images/drt_bg.jpg";
 $page_title = "DTR Project Acer";
 $logo = "images/HSN.png";
+
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    // RESTRICTIONS
+    if (empty($email) || empty($password)) {
+        $error = "Email and password are required.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = "Please enter a valid email address.";
+    } elseif (strlen($password) < 8) {
+        $error = "Password must be at least 8 characters.";
+    } else {
+        $_SESSION['user_email'] = $email;
+        header("Location: main_page.php");
+        exit();
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,38 +32,43 @@ $logo = "images/HSN.png";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
     <link rel="stylesheet" href="index.css">
-   
-   <style>
+    <style>
         body::before {
             background-image: url('<?php echo $bg_image; ?>');
         }
     </style>
-
 </head>
 <body>
 
     <div class="container">
         <img src="<?php echo $logo; ?>" alt="HSN Logo" class="logo">
         <h2>Sign In</h2>
+        <?php if (!empty($error)): ?>
+    <div class="error-msg">
+        <?php echo $error; ?>
+    </div>
+<?php endif; ?>
 
-        <div class="field-group">
-            <label>Email</label>
-            <input type="email" placeholder="Enter your email">
-        </div>
+        <form action="index.php" method="POST">
+            <div class="field-group">
+                <label>Email</label>
+                <input type="email" name="email" placeholder="Enter your email">
+            </div>
 
-        <div class="field-group">
-            <label>Password</label>
-            <input type="password" placeholder="Enter your password">
-        </div>
+            <div class="field-group">
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Enter your password">
+            </div>
 
-        <div class="remember-row">
-            <label class="remember-me">
-                <input type="checkbox"> Remember me
-            </label>
-            <a href="https://www.hsnservice.com/" target="_blank" class="forgot-password"><u>Forgot password?</u></a>
-        </div>
+            <div class="remember-row">
+                <label class="remember-me">
+                    <input type="checkbox"> Remember me
+                </label>
+                <a href="https://www.hsnservice.com/" target="_blank" class="forgot-password"><u>Forgot password?</u></a>
+            </div>
 
-        <button class="btn-signin">Sign In</button>
+            <button type="submit" class="btn-signin">Sign In</button>
+        </form>
 
         <div class="no-account-group">
             <label>Don't have an HSN ID? </label>
