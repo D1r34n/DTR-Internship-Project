57521cef82cf71dab61267170e08071c198a3800
Earl <earl3jordan@gmail.com>
2026-04-24 17:22:21 +0800

    to be merged sql

---
+++ sql/added_leave_reqs_Earl.sql
@@ -0,0 +1,334 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2026 at 11:20 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dtr_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendances`
+--
+
+CREATE TABLE `attendances` (
+  `id` bigint(20) NOT NULL,
+  `employee_id` bigint(20) NOT NULL,
+  `work_date` date NOT NULL,
+  `scheduled_start_datetime` datetime NOT NULL,
+  `scheduled_end_datetime` datetime NOT NULL,
+  `actual_time_in` datetime DEFAULT NULL,
+  `actual_time_out` datetime DEFAULT NULL,
+  `total_work_hours` decimal(10,2) DEFAULT 0.00,
+  `late_minutes` int(11) DEFAULT 0,
+  `undertime_minutes` int(11) DEFAULT 0,
+  `overtime_minutes` int(11) DEFAULT 0,
+  `status` enum('present','absent','incomplete','late','undertime') DEFAULT 'incomplete',
+  `overtime_status` enum('none','pending','approved') DEFAULT 'none',
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendances`
+--
+
+INSERT INTO `attendances` (`id`, `employee_id`, `work_date`, `scheduled_start_datetime`, `scheduled_end_datetime`, `actual_time_in`, `actual_time_out`, `total_work_hours`, `late_minutes`, `undertime_minutes`, `overtime_minutes`, `status`, `overtime_status`, `updated_at`) VALUES
+(1, 5, '2026-04-23', '2026-04-23 08:30:00', '2026-04-23 17:30:00', '2026-04-23 14:44:55', '2026-04-23 15:55:36', 1.18, 375, 94, 0, 'late', 'none', '2026-04-23 07:55:36'),
+(5, 5, '2026-04-24', '2026-04-24 08:30:00', '2026-04-24 17:30:00', '2026-04-24 08:45:22', '2026-04-24 09:08:46', 0.39, 15, 501, 0, 'late', 'none', '2026-04-24 01:08:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('admin','employee','','') NOT NULL DEFAULT 'employee'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`id`, `name`, `email`, `password`, `role`) VALUES
+(2, 'User', 'user@gmail.com', 'user123', 'employee'),
+(3, 'Admin', 'admin@gmail.com', 'admin123', 'admin'),
+(4, 'User1', 'user1@gmail.com', 'user123', 'employee'),
+(5, 'Earl', 'earl@gmail.com', '123', 'employee');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leave_requests`
+--
+
+CREATE TABLE `leave_requests` (
+  `id` int(11) NOT NULL,
+  `employee_id` int(11) DEFAULT NULL,
+  `leave_type` varchar(50) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `selected_dates` text DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `status` varchar(20) DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `leave_requests`
+--
+
+INSERT INTO `leave_requests` (`id`, `employee_id`, `leave_type`, `start_date`, `end_date`, `selected_dates`, `reason`, `status`, `created_at`) VALUES
+(4, 5, 'sick leave', '2026-04-20', '2026-04-23', NULL, 'im sick boss', 'approved', '2026-04-24 08:39:49'),
+(5, 5, 'birthday leave', '2026-04-27', '2026-04-27', NULL, 'imma bday boss', 'approved', '2026-04-24 08:40:56'),
+(6, 5, 'solo parent leave', '2026-04-28', '2026-04-29', NULL, 'its my graduation og my son and daughter boss', 'approved', '2026-04-24 08:41:44'),
+(7, 5, 'vacation leave', '2026-04-30', '2026-05-08', NULL, 'Pagod nko boss', 'approved', '2026-04-24 08:45:21'),
+(8, 5, 'solo parent leave', '2026-05-04', '2026-05-05', NULL, 'Enrollment of my sons', 'approved', '2026-04-24 08:55:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` bigint(20) NOT NULL,
+  `employee_id` bigint(20) NOT NULL,
+  `log_type` enum('IN','OUT') NOT NULL,
+  `log_time` datetime NOT NULL,
+  `longitude` decimal(10,7) NOT NULL,
+  `latitude` decimal(10,7) NOT NULL,
+  `accuracy` float DEFAULT NULL,
+  `is_within_office` tinyint(1) NOT NULL DEFAULT 0,
+  `distance_meters` float DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`id`, `employee_id`, `log_type`, `log_time`, `longitude`, `latitude`, `accuracy`, `is_within_office`, `distance_meters`, `created_at`) VALUES
+(69, 2, 'IN', '2026-04-22 17:03:44', 120.9954933, 14.5842660, 55, 1, 21.954, '2026-04-22 09:03:44'),
+(70, 2, 'OUT', '2026-04-22 17:04:07', 139.6917060, 35.6894870, 150, 0, 2996100, '2026-04-22 09:04:07'),
+(71, 5, 'IN', '2026-04-23 14:44:55', 120.9954840, 14.5842724, 55, 1, 22.096, '2026-04-23 06:44:55'),
+(72, 5, 'OUT', '2026-04-23 14:45:49', 120.9954720, 14.5842811, 55, 1, 22.3536, '2026-04-23 06:45:49'),
+(73, 5, 'IN', '2026-04-23 14:45:55', 120.9954720, 14.5842811, 55, 1, 22.3536, '2026-04-23 06:45:55'),
+(74, 5, 'OUT', '2026-04-23 15:24:28', 120.9954851, 14.5842742, 55, 1, 21.8681, '2026-04-23 07:24:28'),
+(75, 5, 'IN', '2026-04-23 15:24:44', 120.9954851, 14.5842742, 55, 1, 21.8681, '2026-04-23 07:24:44'),
+(76, 5, 'OUT', '2026-04-23 15:55:36', 120.9954660, 14.5842875, 55, 1, 22.3828, '2026-04-23 07:55:36'),
+(77, 5, 'IN', '2026-04-23 15:55:46', 120.9954660, 14.5842875, 55, 1, 22.3828, '2026-04-23 07:55:46'),
+(78, 5, 'OUT', '2026-04-24 08:45:17', 120.9954952, 14.5842675, 55, 1, 21.6908, '2026-04-24 00:45:17'),
+(79, 5, 'IN', '2026-04-24 08:45:22', 120.9954952, 14.5842675, 55, 1, 21.6908, '2026-04-24 00:45:22'),
+(80, 5, 'OUT', '2026-04-24 09:08:31', 120.9954751, 14.5842789, 55, 1, 22.2745, '2026-04-24 01:08:31'),
+(81, 5, 'IN', '2026-04-24 09:08:40', 120.9954751, 14.5842789, 55, 1, 22.2745, '2026-04-24 01:08:40'),
+(82, 5, 'OUT', '2026-04-24 09:08:46', 120.9954751, 14.5842789, 55, 1, 22.2745, '2026-04-24 01:08:46'),
+(83, 5, 'IN', '2026-04-24 09:08:54', 120.9954751, 14.5842789, 55, 1, 22.2745, '2026-04-24 01:08:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `overtime_requests`
+--
+
+CREATE TABLE `overtime_requests` (
+  `id` int(11) NOT NULL,
+  `employee_id` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `time_in` time DEFAULT NULL,
+  `time_out` time DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `status` varchar(20) DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` bigint(20) NOT NULL,
+  `employee_id` bigint(20) NOT NULL,
+  `schedule_date` date NOT NULL,
+  `scheduled_start_datetime` datetime DEFAULT NULL,
+  `scheduled_end_datetime` datetime DEFAULT NULL,
+  `is_rest_day` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `schedules`
+--
+
+INSERT INTO `schedules` (`id`, `employee_id`, `schedule_date`, `scheduled_start_datetime`, `scheduled_end_datetime`, `is_rest_day`) VALUES
+(4, 2, '2026-04-22', '2026-04-22 07:20:00', '2026-04-22 16:22:00', 0),
+(5, 2, '2026-04-23', '2026-04-23 07:20:00', '2026-04-23 16:22:00', 0),
+(6, 2, '2026-04-24', '2026-04-24 07:20:00', '2026-04-24 16:22:00', 0),
+(7, 2, '2026-04-25', NULL, NULL, 1),
+(8, 2, '2026-04-26', NULL, NULL, 1),
+(9, 2, '2026-04-27', '2026-04-27 07:20:00', '2026-04-27 16:22:00', 0),
+(10, 2, '2026-04-28', '2026-04-28 07:20:00', '2026-04-28 16:22:00', 0),
+(23, 5, '2026-04-27', '2026-04-27 08:30:00', '2026-04-27 17:30:00', 0),
+(24, 5, '2026-04-28', '2026-04-28 08:30:00', '2026-04-28 17:30:00', 0),
+(25, 5, '2026-04-29', '2026-04-29 08:30:00', '2026-04-29 17:30:00', 0),
+(26, 5, '2026-04-30', '2026-04-30 08:30:00', '2026-04-30 17:30:00', 0),
+(27, 5, '2026-05-01', '2026-05-01 08:30:00', '2026-05-01 17:30:00', 0),
+(28, 5, '2026-04-23', '2026-04-23 08:30:00', '2026-04-23 17:30:00', 0),
+(29, 5, '2026-04-24', '2026-04-24 08:30:00', '2026-04-24 17:30:00', 0),
+(30, 5, '2026-04-22', '2026-04-22 08:30:00', '2026-04-22 17:30:00', 0),
+(31, 5, '2026-04-21', '2026-04-21 08:30:00', '2026-04-21 17:30:00', 0),
+(32, 5, '2026-04-20', '2026-04-20 08:30:00', '2026-04-20 17:30:00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_state`
+--
+
+CREATE TABLE `system_state` (
+  `key_name` varchar(100) NOT NULL,
+  `value` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_state`
+--
+
+INSERT INTO `system_state` (`key_name`, `value`) VALUES
+('attendance_last_finalize', '2026-04-21');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendances`
+--
+ALTER TABLE `attendances`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uniq_employee_workdate` (`employee_id`,`work_date`),
+  ADD KEY `idx_employee_date` (`employee_id`,`work_date`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `leave_requests`
+--
+ALTER TABLE `leave_requests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employee_id` (`employee_id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_employee_time` (`employee_id`,`log_time`),
+  ADD KEY `idx_log_type` (`log_type`);
+
+--
+-- Indexes for table `overtime_requests`
+--
+ALTER TABLE `overtime_requests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employee_id` (`employee_id`);
+
+--
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_employee_date` (`employee_id`,`schedule_date`);
+
+--
+-- Indexes for table `system_state`
+--
+ALTER TABLE `system_state`
+  ADD PRIMARY KEY (`key_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendances`
+--
+ALTER TABLE `attendances`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `leave_requests`
+--
+ALTER TABLE `leave_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
+
+--
+-- AUTO_INCREMENT for table `overtime_requests`
+--
+ALTER TABLE `overtime_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `leave_requests`
+--
+ALTER TABLE `leave_requests`
+  ADD CONSTRAINT `leave_requests_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`);
+
+--
+-- Constraints for table `overtime_requests`
+--
+ALTER TABLE `overtime_requests`
+  ADD CONSTRAINT `overtime_requests_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
