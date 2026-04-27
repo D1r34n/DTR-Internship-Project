ea862fc203d95ff19f43de7796c42f1275686669
Raiden <edrian.evangelista@gmail.com>
2026-04-27 09:14:37 +0800

    Update dtr_system.sql

--- sql/dtr_system.sql
+++ sql/dtr_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2026 at 02:46 AM
+-- Generation Time: Apr 27, 2026 at 03:11 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -40,6 +40,7 @@ CREATE TABLE `attendances` (
   `late_minutes` int(10) UNSIGNED DEFAULT 0,
   `undertime_minutes` int(10) UNSIGNED DEFAULT 0,
   `overtime_minutes` int(10) UNSIGNED DEFAULT 0,
+  `break_minutes` int(10) UNSIGNED DEFAULT 0,
   `status` enum('present','absent','incomplete') DEFAULT 'incomplete',
   `overtime_status` enum('none','pending','approved','rejected') DEFAULT 'none',
   `missed_time_out` tinyint(1) DEFAULT 0,
@@ -51,8 +52,8 @@ CREATE TABLE `attendances` (
 -- Dumping data for table `attendances`
 --
 
-INSERT INTO `attendances` (`id`, `employee_id`, `schedule_id`, `work_date`, `scheduled_start`, `scheduled_end`, `actual_time_in`, `actual_time_out`, `total_work_minutes`, `late_minutes`, `undertime_minutes`, `overtime_minutes`, `status`, `overtime_status`, `missed_time_out`, `created_at`, `updated_at`) VALUES
-(3, 2, 62, '2026-04-27', '2026-04-27 00:00:00', '2026-04-27 09:00:00', NULL, NULL, 0, 0, 0, 0, 'incomplete', 'none', 0, '2026-04-27 00:42:36', '2026-04-27 00:42:36');
+INSERT INTO `attendances` (`id`, `employee_id`, `schedule_id`, `work_date`, `scheduled_start`, `scheduled_end`, `actual_time_in`, `actual_time_out`, `total_work_minutes`, `late_minutes`, `undertime_minutes`, `overtime_minutes`, `break_minutes`, `status`, `overtime_status`, `missed_time_out`, `created_at`, `updated_at`) VALUES
+(3, 2, 62, '2026-04-27', '2026-04-27 00:00:00', '2026-04-27 09:00:00', NULL, NULL, 0, 0, 0, 0, NULL, 'incomplete', 'none', 0, '2026-04-27 00:42:36', '2026-04-27 00:42:36');
 
 -- --------------------------------------------------------
 
@@ -117,7 +118,7 @@ INSERT INTO `leave_requests` (`id`, `employee_id`, `leave_type`, `start_date`, `
 CREATE TABLE `logs` (
   `id` bigint(20) NOT NULL,
   `employee_id` bigint(20) NOT NULL,
-  `log_type` enum('IN','OUT') NOT NULL,
+  `log_type` enum('IN','OUT','BREAK_IN','BREAK_OUT') NOT NULL,
   `log_time` datetime NOT NULL,
   `longitude` decimal(10,7) NOT NULL,
   `latitude` decimal(10,7) NOT NULL,
@@ -126,29 +127,6 @@ CREATE TABLE `logs` (
   `distance_meters` float DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `logs`
---
-
-INSERT INTO `logs` (`id`, `employee_id`, `log_type`, `log_time`, `longitude`, `latitude`, `accuracy`, `is_within_office`, `distance_meters`, `created_at`) VALUES
-(69, 2, 'IN', '2026-04-22 17:03:44', 120.9954933, 14.5842660, 55, 1, 21.954, '2026-04-22 09:03:44'),
-(70, 2, 'OUT', '2026-04-22 17:04:07', 139.6917060, 35.6894870, 150, 0, 2996100, '2026-04-22 09:04:07'),
-(71, 5, 'IN', '2026-04-23 14:44:55', 120.9954840, 14.5842724, 55, 1, 22.096, '2026-04-23 06:44:55'),
-(72, 5, 'OUT', '2026-04-23 14:45:49', 120.9954720, 14.5842811, 55, 1, 22.3536, '2026-04-23 06:45:49'),
-(73, 5, 'IN', '2026-04-23 14:45:55', 120.9954720, 14.5842811, 55, 1, 22.3536, '2026-04-23 06:45:55'),
-(74, 5, 'OUT', '2026-04-23 15:24:28', 120.9954851, 14.5842742, 55, 1, 21.8681, '2026-04-23 07:24:28'),
-(75, 5, 'IN', '2026-04-23 15:24:44', 120.9954851, 14.5842742, 55, 1, 21.8681, '2026-04-23 07:24:44'),
-(76, 5, 'OUT', '2026-04-23 15:55:36', 120.9954660, 14.5842875, 55, 1, 22.3828, '2026-04-23 07:55:36'),
-(77, 5, 'IN', '2026-04-23 15:55:46', 120.9954660, 14.5842875, 55, 1, 22.3828, '2026-04-23 07:55:46'),
-(78, 5, 'OUT', '2026-04-24 08:45:17', 120.9954952, 14.5842675, 55, 1, 21.6908, '2026-04-24 00:45:17'),
-(79, 5, 'IN', '2026-04-24 08:45:22', 120.9954952, 14.5842675, 55, 1, 21.6908, '2026-04-24 00:45:22'),
-(80, 5, 'OUT', '2026-04-24 09:08:31', 120.9954751, 14.5842789, 55, 1, 22.2745, '2026-04-24 01:08:31'),
-(81, 5, 'IN', '2026-04-24 09:08:40', 120.9954751, 14.5842789, 55, 1, 22.2745, '2026-04-24 01:08:40'),
-(82, 5, 'OUT', '2026-04-24 09:08:46', 120.9954751, 14.5842789, 55, 1, 22.2745, '2026-04-24 01:08:46'),
-(83, 5, 'IN', '2026-04-24 09:08:54', 120.9954751, 14.5842789, 55, 1, 22.2745, '2026-04-24 01:08:54'),
-(483, 2, 'IN', '2026-04-20 23:37:09', 120.9954855, 14.5842718, 55, 1, 22.037, '2026-04-20 15:37:09'),
-(484, 2, 'OUT', '2026-04-20 23:37:16', 120.9954855, 14.5842718, 55, 1, 22.037, '2026-04-20 15:37:16');
 
 -- --------------------------------------------------------
 
