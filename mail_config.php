d9657f5fb7973b97c691b94c32c3e7bc4a809317
Raiden <edrian.evangelista@gmail.com>
2026-05-08 14:43:55 +0800

    php mailing system

---
+++ mail_config.php
@@ -0,0 +1,7 @@
+<?php
+define('MAIL_HOST',       'smtp.gmail.com');   // e.g. smtp.gmail.com / sandbox.smtp.mailtrap.io
+define('MAIL_PORT',       587);                // 587 for TLS, 465 for SSL
+define('MAIL_USERNAME',   'edrian.evangelista@gmail.com');   // the Gmail account used to send
+define('MAIL_PASSWORD',   'wtot eavw jggn fzmz');// Gmail App Password (not your login password)
+define('MAIL_FROM_EMAIL', 'edrian.evangelista@gmail.com');   // must match MAIL_USERNAME for Gmail
+define('MAIL_FROM_NAME',  'HSN DTR System');
