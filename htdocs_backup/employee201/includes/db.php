22a2ffd3d02ac08845f16f6dda5a68d3f632d9d8
Laileanna Rachelle R. Del Rosario <lrdelrosario@udm.edu.ph>
2026-05-14 10:58:52 +0800

    yes

--- htdocs_backup/employee201/includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-
-$conn = new mysqli($host, $user, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
--- htdocs_backup/employee201/includes/log.php
+++
@@ -1,7 +0,0 @@
-<?php
-function logAction($conn, $admin_id, $action) {
-    $stmt = $conn->prepare("INSERT INTO logs (admin_id, action, log_time) VALUES (?, ?, NOW())");
-    $stmt->bind_param("is", $admin_id, $action);
-    $stmt->execute();
-}
-?>
