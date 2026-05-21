24e2e6bf4492ce6330239f689975326f302aa121
Raiden <edrian.evangelista@gmail.com>
2026-05-21 16:06:10 +0800

    Update admin_cutoff.php

--- management_pages/admin_cutoff.php
+++ management_pages/admin_cutoff.php
@@ -1,7 +1,7 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) session_start();
 
-if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'admin') {
+if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'superadmin') {
     header("Location: ../index.php");
     exit();
 }
